58dde7625e53f2974458a8dcfc69daa66e4a21bd
SergioViacheslaev <internetwalker@yandex.ru>
2021-09-01 17:31:50 +0300

    Add surefire plugin

--- src/test/java/ru/home/MainTest.java
+++ src/test/java/ru/home/MainTest.java
@@ -1,12 +1,19 @@
 package ru.home;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * @author Sergei Viacheslaev
  */
+
 class MainTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("--------STARTING TESTS !!!-----");
+    }
 
     @Test
     void getHelloMessage() {
